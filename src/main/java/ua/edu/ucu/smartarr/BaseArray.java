aa3156161f074f7be7cc7ad34bbbcb45b5b98266
D1mD1m <kushnir.dmytro.93@gmail.com>
2016-11-19 20:20:05 +0200

    implementation in process1

--- src/main/java/ua/edu/ucu/smartarr/BaseArray.java
+++ src/main/java/ua/edu/ucu/smartarr/BaseArray.java
@@ -1,6 +1,25 @@
 package ua.edu.ucu.smartarr;
 
 // Base array for decorators
-public class BaseArray {
+public class BaseArray implements SmartArray{
+    private Object[] val = null;
 
+    public BaseArray(Object[] val) {
+        this.val = val;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return this.val.clone();
+    }
+
+    @Override
+    public String operationDescription() {
+        return "base array"; //actually  nothing done on this step
+    }
+
+    @Override
+    public int size() {
+        return this.val.length;//returns size of val array
+    }
 }
--- src/main/java/ua/edu/ucu/smartarr/DistinctDecorator.java
+++ src/main/java/ua/edu/ucu/smartarr/DistinctDecorator.java
@@ -1,6 +1,24 @@
 package ua.edu.ucu.smartarr;
 
-// Remove duplicates from SmartArray. Use method equals() to compare objects
-public class DistinctDecorator {
+import java.util.ArrayList;
 
+// Remove duplicates from SmartArray. Use method equals() to compare objects
+public class DistinctDecorator extends SmartArrayDecorator{
+
+    public DistinctDecorator(SmartArray smartArray) {
+        super(smartArray);
+    }
+
+
+    @Override
+    public Object[] toArray() {
+        //Object[] rawArray = this.smartArray.toArray();
+        ArrayList <Object> rawArray = new ArrayList<Object>();
+        return new Object[0];
+    }
+
+    @Override
+    public String operationDescription() {
+        return this.smartArray.operationDescription().concat(" deleted repeatings ");
+    }
 }
